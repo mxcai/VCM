2c42f816a71a359c8320d77dee5ad58504e30b41
mxcai <32085863+mxcai@users.noreply.github.com>
2019-03-04 23:04:52 +0800

    Update PXEM_vs_MM.R

--- demo/PXEM_vs_MM.R
+++ demo/PXEM_vs_MM.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(reshape2)
-n <- 1000
+n <- 500
 d <- 1000
 
 sg2 <- 0.1
@@ -13,11 +13,18 @@ y0 <- X%*%w
 
 y  <- y0 + sqrt(se2)*rnorm(n)
 
-fit_PXEM <- linRegPXEM(X=X,y=y,tol = 1e-6,maxIter =200)
-fit_MM <- linRegMM(X=X,y=y,tol=1e-6,maxIter = 200)
+fit_PXEM <- linRegPXEM(X=X,y=y,tol = 1e-6,maxIter =2000)
+fit_MM <- linRegMM(X=X,y=y,tol=1e-6,maxIter = 2000)
 fit_MoM <- linReg_MoM(X=X,y=y)
 
-lb <- cbind(PXEM=fit_PXEM$lb[-1],MM=fit_MM$lb[-1])
+lb_MM <- fit_MM$lb
+lb_PX <- fit_PXEM$lb
+
+lb_MM <- c(lb_MM,rep(NA,ifelse(length(lb_PX)>length(lb_MM),length(lb_PX)-length(lb_MM),0)))
+lb_PX <- c(lb_PX,rep(NA,ifelse(length(lb_MM)>length(lb_PX),length(lb_MM)-length(lb_PX),0)))
+
+# maxiter <- min(c(length(fit_MM$lb),length(fit_PXEM$lb)))-1
+lb <- cbind(PXEM=lb_PX[2:length(lb_PX)],MM=lb_MM[2:length(lb_MM)])
 
 lb <- melt(lb)
 names(lb) <- c('iter','method','likelihood')
