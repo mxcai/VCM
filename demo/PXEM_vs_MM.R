ce30f9c97f2a2bc9bdf632871d1083c112c449f5
mxcai <32085863+mxcai@users.noreply.github.com>
2019-04-17 10:40:36 +0800

    Update PXEM_vs_MM.R

--- demo/PXEM_vs_MM.R
+++ demo/PXEM_vs_MM.R
@@ -1,40 +1,32 @@
 library(ggplot2)
 library(reshape2)
-n <- 1000
+n <- 500
 d <- 1000
-q <- 5
 
 sg2 <- 0.1
 se2 <- 1
-
 X <- matrix(rnorm(n*d),n,d)
 X <- scale(X)/sqrt(d)
+
 w  <- c(rnorm(d,0,sqrt(sg2)))
 y0 <- X%*%w
 
-Z <- matrix(rnorm(n*q),n,q)
-omega <- c(1:5)
-y1 <- Z%*%omega
+y  <- y0 + sqrt(se2)*rnorm(n)
 
-y  <- y0 + y1 + sqrt(se2)*rnorm(n)
+fit_PXEM <- linRegPXEM(X=X,y=y,tol = 1e-6,maxIter =2000)
+fit_MM <- linRegMM(X=X,y=y,tol=1e-6,maxIter = 2000)
+fit_MoM <- linReg_MoM(X=X,y=y)
 
-# fit PXEM
-fit_PXEM <- linRegPXEM(X=X,y=y,Z = Z,tol = 1e-6,maxIter =200)
-yhat_PX <- predict(fit_PXEM,X,Z)
-mean((yhat_PX-y)^2)
+lb_MM <- fit_MM$lb
+lb_PX <- fit_PXEM$lb
 
-# fit MM
-fit_MM <- linRegMM(X=X,y=y,Z=Z,tol=1e-6,maxIter = 200)
-yhat_MM <- predict(fit_MM,X,Z)
-mean((yhat_MM-y)^2)
+lb_MM <- c(lb_MM,rep(NA,ifelse(length(lb_PX)>length(lb_MM),length(lb_PX)-length(lb_MM),0)))
+lb_PX <- c(lb_PX,rep(NA,ifelse(length(lb_MM)>length(lb_PX),length(lb_MM)-length(lb_PX),0)))
 
-# fit MoM
-fit_MoM <- linReg_MoM(X=X,Z=Z,y=y)
-
-lb <- cbind(PXEM=fit_PXEM$lb[-1],MM=fit_MM$lb[-1])
+# maxiter <- min(c(length(fit_MM$lb),length(fit_PXEM$lb)))-1
+lb <- cbind(PXEM=lb_PX[2:length(lb_PX)],MM=lb_MM[2:length(lb_MM)])
 
 lb <- melt(lb)
 names(lb) <- c('iter','method','likelihood')
 P <- ggplot(lb,aes(x=iter,y=likelihood,color=method)) + geom_line()
 P
-
