b47dc784752127ea43ed203afe7717d1bf19ede0
mxcai <32085863+mxcai@users.noreply.github.com>
2021-07-22 12:34:57 +0800

    include covariates

--- R/cvperf.linReg.R
+++ R/cvperf.linReg.R
@@ -1,5 +1,5 @@
 # Evaluate the performance of bayes regression using cv
-cvperf.linReg <- function(X, y, nfolds=10, alg="PXEM", verbose=F, ...){
+cvperf.linReg <- function(X, y, Z=NULL, nfolds=10, alg="PXEM", verbose=F, ...){
   p <- ncol(X)
   n <- nrow(X)
   # decide the cv assignments
@@ -25,20 +25,23 @@ cvperf.linReg <- function(X, y, nfolds=10, alg="PXEM", verbose=F, ...){
 
     X_train <- X[idx!=i,]
     y_train <- y[idx!=i]
+    Z_train <- Z[idx!=i,]
 
     X_test  <- matrix(X[idx==i,],ncol = p)
     y_test  <- y[idx==i]
+    Z_test  <- Z[idx==i,]
+    if(!is.null(Z)) Z_test <- matrix(Z_test,ncol = ncol(Z))
 
     if(alg == "MoM"){
-      fit <- linRegMoM(X_train,y_train,verbose=verbose,...)
+      fit <- linRegMoM(X_train,y_train,Z_train,verbose=verbose,...)
     } else if(alg == "EM"){
-      fit <- linRegMM(X_train,y_train,verbose=verbose,...)
+      fit <- linRegMM(X_train,y_train,Z_train,verbose=verbose,...)
     } else if(alg == "PXEM"){
-      fit <- linRegPXEM(X_train,y_train,verbose=verbose,...)
+      fit <- linRegPXEM(X_train,y_train,Z_train,verbose=verbose,...)
     }
 
 
-    testErr[i]  <- mean((y_test-predict(fit,X_test))^2)
+    testErr[i]  <- mean((y_test-predict(fit,X_test,Z_test))^2)
     R2[i]       <- 1 - testErr[i] / mean((y_test-mean(y_train))^2)
   }
   cvm  <- mean(testErr)
