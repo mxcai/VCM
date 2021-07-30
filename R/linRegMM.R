ba0a6d6370c43db4d05d1673610de71f465219eb
mxcai <32085863+mxcai@users.noreply.github.com>
2021-07-30 10:19:36 +0800

    correct typo in comment

--- R/linRegMM.R
+++ R/linRegMM.R
@@ -68,7 +68,7 @@ linRegMM <- function(X,y,Z=NULL,maxIter=1500,tol=1e-6,se2=NULL,sb2=NULL,verbose=
   if(ncol(Z)==1){
     beta0 <- beta0 + ym - colSums(mu*Xm)
   } else{
-    beta0 <- beta0 + solve(ZZ,colSums(Z)*(ym-sum(mu*Xm)))  # = beta0 - (Z^TZ)^-1Z^T[(y^bar-X^bar%*%mu),...,(y^bar-X^bar%*%mu)]
+    beta0 <- beta0 + solve(ZZ,colSums(Z)*(ym-sum(mu*Xm)))  # = beta0 + (Z^TZ)^-1Z^T[(y^bar-X^bar%*%mu),...,(y^bar-X^bar%*%mu)]
   }
 
 
